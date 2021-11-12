7ee44cbf5f10fcd4c12744111ce088bdbe81bd92
Nickolai Vakhrushev <nickv@email.com>
2021-11-12 16:43:42 +0700

    add pytest parametrize for test promo=offerN links

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,10 +1,12 @@
 from pages.product_page import ProductPage
-import time
+import pytest
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
 
 
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('link', [pytest.param(base_link + str(num), marks=pytest.mark.xfail) if num == 7
+                                  else base_link + str(num) for num in range(10)])
+def test_guest_can_add_product_to_basket(browser, link):
     page = ProductPage(browser, link)
     page.open()
     page.add_protuct_to_backet()
