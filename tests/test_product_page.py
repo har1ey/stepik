b022ddaa6d8c882b28b2685af689165787798433
Nickolai Vakhrushev <nickv@email.com>
2021-11-12 18:27:03 +0700

    fix test test_guest_can_go_to_login_page_from_product_page()

--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,4 +1,5 @@
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 import pytest
 
 base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer"
@@ -48,3 +49,5 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
