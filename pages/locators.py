cfe02badc47f76d33fae87e25f9f1e7395a99366
Nickolai Vakhrushev <nickv@email.com>
2021-11-12 16:06:04 +0700

    add product_page and test_product_page logic

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_LINK = (By.ID, "login_form")
     REGISTER_LINK = (By.ID, "register_form")
+
+
+class ProductPageLocators:
+    BTN_ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form > button")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
+    PRODUCT_DESCRIPTION = (By.CSS_SELECTOR, "#product_description+p")
+    SUCCESS_MESSAGES = (By.CSS_SELECTOR, ".alertinner strong")
---
+++ pages/product_page.py
@@ -0,0 +1,42 @@
+from pages.base_page import BasePage
+from pages.locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    product_name = ''
+    product_price = ''
+
+    def add_protuct_to_backet(self):
+        self.should_be_name()
+        self.should_be_price()
+        self.should_be_add_button()
+
+        btn = self.browser.find_element(*ProductPageLocators.BTN_ADD_TO_BASKET)
+        btn.click()
+        self.solve_quiz_and_get_code()
+
+        self.should_be_success()
+        self.check_success_message()
+
+    def should_be_name(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_NAME), "Name of product not found"
+        self.product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+
+    def should_be_price(self):
+        assert self.is_element_present(*ProductPageLocators.PRODUCT_PRICE), "Price of product not found"
+        self.product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+
+    def should_be_add_button(self):
+        assert self.is_element_present(*ProductPageLocators.BTN_ADD_TO_BASKET), "Button 'Add to basket' is not " \
+                                                                                "presented "
+
+    def should_be_success(self):
+        assert self.is_element_present(*ProductPageLocators.SUCCESS_MESSAGES), "Message of Success added product in " \
+                                                                                       "basket not found "
+
+    def check_success_message(self):
+        msg_lst = self.browser.find_elements(*ProductPageLocators.SUCCESS_MESSAGES)
+        assert len(msg_lst) == 3, "Success message not found"
+
+        assert self.product_name == msg_lst[0].text, "Wrong name product added to basket"
+        assert self.product_price == msg_lst[2].text, "Wrong price product added to basket"
